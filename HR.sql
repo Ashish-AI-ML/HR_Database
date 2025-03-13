eee9a941306803b5f5c0e504b4d4057e06cf13e9
Ashish chaturvedi <ashishchaturvedi758@gmail.com>
2025-03-13 15:10:51 +0530

    Add files via upload

---
+++ HR.sql
@@ -0,0 +1,14 @@
+use HR
+
+ select * from employees where employee_id =100
+ 
+ --retrieve the names , empid of employees who work for department marketing
+ --select distict job id
+ --retrieve the name of the employees and their coresponding department name
+ --for each emply retreive emp id ,name salary and depandants name
+
+
+ select * from departments 
+ select * from employees
+
+ select first_name,employee__id from employees where departments__id=9
